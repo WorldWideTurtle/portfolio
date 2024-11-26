21c0b1ebf9c1f7175c574f4397ba7a12d53f7f2f
WorldWideTurtle <148323035+WorldWideTurtle@users.noreply.github.com>
2024-11-26 11:35:08 +0100

    Add JS transition to Opacity and Gradient Transition is currently Frame dependant

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,7 @@
 
 import { MutableRefObject, useEffect, useRef } from "react"
 import styles from "./header.module.css"
+import Link from "next/link"
 
 const links = [
     ["about"],
@@ -12,37 +13,67 @@ const links = [
 
 export default function Header() {
     let navbar : MutableRefObject<HTMLHeadingElement | null> = useRef(null)
-    
+
     useEffect(()=>{
-        let lastUpdate = 0;
-        let updateOpacity = () => {
-            if (navbar.current === null) return;
+        if (navbar.current === null) return;
 
-            let buffer = 10;
+        let buffer = 10;
+        let maxOffset = 100;
+
+        let getCurrentRatio = () => {
             let scrollOffset = Math.max(0,window.scrollY - buffer);
-            let maxOffset = 75;
-            let ratio = Math.min(1,scrollOffset / maxOffset)
-
-            if (ratio === lastUpdate) return;
-            lastUpdate = ratio;
-            navbar.current.style.setProperty("--stop",(ratio * 95).toString() + "%")
-            navbar.current.style.setProperty("--tw-bg-opacity",(ratio * .8).toString());
+            return Math.min(1,scrollOffset / maxOffset);
         }
 
-        updateOpacity();
+        let current = getCurrentRatio();
+        let target = current;
 
-        document.addEventListener("scroll",updateOpacity)
+        let updateScrollRatio = () => {
+            let ratio = getCurrentRatio()
+            if (ratio !== target) target = ratio;
+        }
 
-        return () => document.removeEventListener("scroll",updateOpacity)
+        let updateNavVisual = () => {
+            if (navbar.current === null) return;
+
+            navbar.current.style.setProperty("--stop",(current * 90).toString() + "%")
+            navbar.current.style.setProperty("--tw-bg-opacity",(current * .7).toString());
+        }
+
+        let canceled = false;
+        let tween = () => {
+            if (canceled) return;
+
+            current += (target - current) * 0.15;
+            let isDirty = current !== target;
+            if (isDirty && Math.abs(current - target) < 0.01) {
+                current = target;
+                isDirty = true;
+            }
+
+            isDirty && updateNavVisual();
+
+            requestAnimationFrame(tween)
+        }
+
+        updateScrollRatio();
+        tween();
+
+        document.addEventListener("scroll",updateScrollRatio)
+
+        return () => {
+            document.removeEventListener("scroll",updateScrollRatio);
+            canceled = true;
+        }
     }, [])
 
     return (
-        <header ref={navbar} className={"w-full bg-primary bg-opacity-0 flex justify-between py-1 md:py-2 text-xl md:text-2xl lg:text-3xl fixed z-[9999] after:w-full after:h-[2px] after:absolute after:bottom-[-1px] " + styles.base}>
+        <header ref={navbar} className={"w-full bg-primary bg-opacity-0 flex justify-between py-1 md:py-2 text-xl md:text-2xl lg:text-3xl fixed z-[9999] after:w-full after:h-px after:absolute after:bottom-0 backdrop-blur-sm " + styles.base}>
             <h1 className="pl-6">WorldWideTurtle</h1>
             <nav className="hidden md:inline pr-6">
                 <ul className="flex gap-6">
                     {links.map(e=>(
-                        <li key={e[0]}><a href={`#${e[0]}`}>{e[0]}</a></li>
+                        <li key={e[0]}><Link href={`#${e[0]}`}>{e[0]}</Link></li>
                     ))}
                 </ul>
             </nav>
