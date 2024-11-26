7d2134569057f0f14a368da342fa7a41b7018fcf
WorldWideTurtle <148323035+WorldWideTurtle@users.noreply.github.com>
2024-11-26 16:55:55 +0100

    Add configs for better setting managment

---
+++ src/config/header.config.ts
@@ -0,0 +1,39 @@
+type HeaderConfig = {
+    title: string,
+    links: Array<{
+        name: string,
+        href: string
+    }>
+    clearance: number,
+    pixelsToMax: number,
+    transitionDuration: number,
+    timingFunction: (x : number) => number
+}
+
+const headerConfig : HeaderConfig = {
+    title: "WorldWideTurtle",
+    links: [
+        {
+            name:"About",
+            href:"#about"
+        },
+        {
+            name:"Skills",
+            href:"#skills"
+        },
+        {
+            name:"Projects",
+            href:"#projects"
+        },
+        {
+            name:"Contact",
+            href:"#contact"
+        }
+    ],
+    clearance: 10,
+    pixelsToMax: 20,
+    transitionDuration: 500,
+    timingFunction: (x : number) : number => 1 - Math.pow(1 - x, 3)
+}
+
+export default headerConfig
---
+++ src/config/skills.config.ts
@@ -0,0 +1,89 @@
+type SkillsConfig = {
+    skills: Array<{
+        skillName: string,
+        iconName: string,
+        iconColor: string
+    }>
+}
+
+const skillsConfig : SkillsConfig = {
+    skills: [
+        {
+            skillName: "HTML",
+            iconName: "html",
+            iconColor: "#E14E1D"
+        },
+        {
+            skillName: "JavaScript",
+            iconName: "javascript",
+            iconColor: "#F0DB4F"
+        },
+        {
+            skillName: "CSS",
+            iconName: "css",
+            iconColor: "#663399"
+        },
+        {
+            skillName: "TailwindCSS",
+            iconName: "tailwind",
+            iconColor: "#32B1C1"
+        },
+        {
+            skillName: "React",
+            iconName: "react",
+            iconColor: "#00D8FF"
+        },
+        {
+            skillName: "NextJS",
+            iconName: "nextjs",
+            iconColor: "#ffffff"
+        },
+        {
+            skillName: "NodeJS",
+            iconName: "nodejs",
+            iconColor: "#81CD39"
+        },
+        {
+            skillName: "ExpressJS",
+            iconName: "express",
+            iconColor: "#ffffff"
+        },
+        {
+            skillName: "Figma",
+            iconName: "figma",
+            iconColor: "#0ACF83"
+        },
+        {
+            skillName: "GitHub",
+            iconName: "github",
+            iconColor: "#ffffff"
+        },
+        {
+            skillName: "Blender",
+            iconName: "blender",
+            iconColor: "#EA7600"
+        },
+        {
+            skillName: "AutoCAD",
+            iconName: "autocad",
+            iconColor: "#BF2223"
+        },
+        {
+            skillName: "Discord",
+            iconName: "discord",
+            iconColor: "#5865F2"
+        },
+        {
+            skillName: "Illustrator",
+            iconName: "illustrator",
+            iconColor: "#FF9A00"
+        },
+        {
+            skillName: "Obsidian",
+            iconName: "obsidian",
+            iconColor: "#ffffff"
+        }
+    ]
+}
+
+export default skillsConfig;
