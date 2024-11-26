88a92a89a61e7c7cae86470b0273e26d5ffb5c76
WorldWideTurtle <148323035+WorldWideTurtle@users.noreply.github.com>
2024-11-26 19:53:01 +0100

    Remove Fog from Skills Add interactive tilt on hover and click to skills Add knowledge stat to skills to affect scale of skill

---
+++ src/components/skillLantern.tsx
@@ -0,0 +1,85 @@
+'use client'
+
+import LanternIcon from "@/icons/Lantern.svg"
+import ChainIcon from "@/icons/Chain.svg"
+import skillsConfig, { Skill } from "@/config/skills.config"
+import { MutableRefObject, useEffect, useRef } from "react"
+
+type SkillLanternProps = {
+    icon: string,
+    skill: Skill
+}
+
+export default function SkillLantern(props : SkillLanternProps) {
+    let baseIconSize = skillsConfig.baseIconSize;
+    let baseBackgroundSize = skillsConfig.baseBackgroundSize;
+    let backgroundSizeOffset = skillsConfig.backgroundSizeOffset;
+
+    let skill = props.skill;
+    let skillElement : MutableRefObject<HTMLDivElement | null> = useRef(null)
+
+    useEffect(()=>{
+        if (skillElement.current === null) return;
+
+        let isHovered = false;
+        let hoverTimeOut : NodeJS.Timeout | null = null;
+        let hoverHandler = () => {
+            if (isHovered) return;
+            isHovered = true;
+            tiltLantern();
+        }
+
+        let tiltLantern = () => {
+            if (skillElement.current === null) return;
+            let side = Math.random() > 0.5 ? 1 : -1
+            let num = Math.random() * skillsConfig.maxTilt;
+            skillElement.current.style.rotate = (num * side).toString() + "deg"
+            hoverTimeOut = setTimeout(()=>{
+                if (skillElement.current === null) return;
+
+                skillElement.current.style.rotate = "0deg"
+            }, skillsConfig.tiltDuration + 50)
+        }
+
+        let hoverEndHandler = () => {
+            isHovered = false;
+        }
+
+        skillElement.current.addEventListener("mouseenter",hoverHandler)
+        skillElement.current.addEventListener("mouseleave",hoverEndHandler)
+        skillElement.current.addEventListener("click",tiltLantern)
+
+        return () => {
+            if (skillElement.current === null) return;
+
+            skillElement.current.removeEventListener("mouseenter",hoverHandler)
+            skillElement.current.removeEventListener("mouseleave",hoverEndHandler)
+            skillElement.current.removeEventListener("click",tiltLantern)
+            if (hoverTimeOut !== null) clearTimeout(hoverTimeOut)
+        }
+    })
+
+    return (
+        <>
+            <div ref={skillElement} className="origin-top grid relative transition-[rotate]" style={{
+                width: (baseIconSize + skill.knowledge / 2).toString() + "vmax",
+                transitionDuration: skillsConfig.tiltDuration.toString() + "ms",
+                transitionTimingFunction: skillsConfig.tiltEasing
+            }}>
+                <LanternIcon className="fill-primary-light w-full col-start-1 row-start-1"/>
+                <div className="col-start-1 row-start-1 w-[85%] place-self-center z-10 translate-y-[-30%]" dangerouslySetInnerHTML={{ __html: props.icon}}/>
+                <div className="absolute place-self-center z-[5] translate-y-[-15%]" style={{
+                    background: `radial-gradient(circle at center, ${skill.iconColor + "30"}, ${skill.iconColor + "00"} 70%)`,
+                    width: (baseBackgroundSize + skill.knowledge / 2).toString() + "vmax",
+                    height: (baseBackgroundSize + skill.knowledge / 2).toString() + "vmax"
+                }}/>
+                <div className="absolute place-self-center -z-10 translate-y-[-15%]" style={{
+                    background: `radial-gradient(circle at center, ${skill.iconColor + "a0"}, ${skill.iconColor + "00"} 70%)`,
+                    width: (baseBackgroundSize + backgroundSizeOffset + skill.knowledge / 2).toString() + "vmax",
+                    height: (baseBackgroundSize + backgroundSizeOffset + skill.knowledge / 2).toString() + "vmax"
+                }}/>
+            </div>
+            <ChainIcon className="absolute top-[8%] w-[25%] translate-y-[-100%] left-[50%] z-[4] translate-x-[-50%]" />
+        </>
+    )
+}
--- src/config/skills.config.ts
+++ src/config/skills.config.ts
@@ -1,87 +1,119 @@
+type Skill = {
+    skillName: string,
+    iconName: string,
+    iconColor: string,
+    knowledge: 1 | 2 | 3 | 4 | 5
+}
+
+export type {Skill}
+
 type SkillsConfig = {
-    skills: Array<{
-        skillName: string,
-        iconName: string,
-        iconColor: string
-    }>
+    baseIconSize: number,
+    baseBackgroundSize: number,
+    backgroundSizeOffset: number,
+    maxTilt: number,
+    tiltDuration: number,
+    tiltEasing: string,
+    skills: Array<Skill>
 }
 
 const skillsConfig : SkillsConfig = {
+    baseIconSize: 3,
+    baseBackgroundSize: 5.5,
+    backgroundSizeOffset: 2,
+    maxTilt: 10,
+    tiltDuration: 400,
+    tiltEasing: "cubic-bezier(0.25, 0.46, 0.45, 0.94)",
     skills: [
         {
             skillName: "HTML",
             iconName: "html",
-            iconColor: "#E14E1D"
+            iconColor: "#E14E1D",
+            knowledge: 5
         },
         {
             skillName: "JavaScript",
             iconName: "javascript",
-            iconColor: "#F0DB4F"
+            iconColor: "#F0DB4F",
+            knowledge: 5
         },
         {
             skillName: "CSS",
             iconName: "css",
-            iconColor: "#663399"
+            iconColor: "#663399",
+            knowledge: 5
         },
         {
             skillName: "TailwindCSS",
             iconName: "tailwind",
-            iconColor: "#32B1C1"
+            iconColor: "#32B1C1",
+            knowledge: 3
         },
         {
             skillName: "React",
             iconName: "react",
-            iconColor: "#00D8FF"
+            iconColor: "#00D8FF",
+            knowledge: 2
         },
         {
             skillName: "NextJS",
             iconName: "nextjs",
-            iconColor: "#ffffff"
+            iconColor: "#ffffff",
+            knowledge: 2
         },
         {
             skillName: "NodeJS",
             iconName: "nodejs",
-            iconColor: "#81CD39"
+            iconColor: "#81CD39",
+            knowledge: 3
         },
         {
             skillName: "ExpressJS",
             iconName: "express",
-            iconColor: "#ffffff"
+            iconColor: "#ffffff",
+            knowledge: 2
         },
         {
             skillName: "Figma",
             iconName: "figma",
-            iconColor: "#0ACF83"
+            iconColor: "#0ACF83",
+            knowledge: 3
         },
         {
             skillName: "GitHub",
             iconName: "github",
-            iconColor: "#ffffff"
+            iconColor: "#ffffff",
+            knowledge: 3
         },
         {
             skillName: "Blender",
             iconName: "blender",
-            iconColor: "#EA7600"
+            iconColor: "#EA7600",
+            knowledge: 2
         },
         {
             skillName: "AutoCAD",
             iconName: "autocad",
-            iconColor: "#BF2223"
+            iconColor: "#BF2223",
+            knowledge: 3
         },
         {
             skillName: "Discord",
             iconName: "discord",
-            iconColor: "#5865F2"
+            iconColor: "#5865F2",
+            knowledge: 3
         },
         {
             skillName: "Illustrator",
             iconName: "illustrator",
-            iconColor: "#FF9A00"
+            iconColor: "#FF9A00",
+            knowledge: 3
         },
         {
             skillName: "Obsidian",
             iconName: "obsidian",
-            iconColor: "#ffffff"
+            iconColor: "#ffffff",
+            knowledge: 4
         }
     ]
 }
