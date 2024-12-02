ff8ffa3694b090c64bb06efa7236d3490446b300
WorldWideTurtle <sebastian.eckhard@gmail.com>
2024-12-02 23:04:24 +0100

    Add scroll link to skills and Scroll-Margin
    - Scroll-Margin using Translate as opera
    appears to not respect css scroll-margin

--- src/components/Sections/about.tsx
+++ src/components/Sections/about.tsx
@@ -1,12 +1,10 @@
 export default function About() {
     return (
-        <div>
-            <h1 id="about" className="sr-only">About</h1>
-            <div className="px-8 pb-4 relative">
-                <div className="max-w-text tracking-[0.075em] md:text-xl text-justify m-auto">
-                    <span>Sebastian Eckhard (HE/HIM) - </span>
-                    <span className="text-white-600 font-math">I love building experiences that impress, not only visually, but also on a technical level. When I'm not working on my own projects, I like to take pictures, read or play games.</span>
-                </div>
+        <div className="px-8 pb-4 relative">
+            <h1 id="about" className="sr-only translate-y-[-8rem]">About</h1>
+            <div className="max-w-text tracking-[0.075em] md:text-xl text-justify m-auto">
+                <span>Sebastian Eckhard (HE/HIM) - </span>
+                <span className="text-white-600 font-math">I love building experiences that impress, not only visually, but also on a technical level. When I'm not working on my own projects, I like to take pictures, read or play games.</span>
             </div>
         </div>
     )
