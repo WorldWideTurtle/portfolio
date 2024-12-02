23704dee4fb4335a0ad4c43fb871e98bf4b5dccf
WorldWideTurtle <sebastian.eckhard@gmail.com>
2024-12-02 22:57:42 +0100

    Update README and Navigation Bar
    - Remove README template code
    
    - Add hover state to Navigation
    - Change padding and text size

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -76,12 +76,12 @@ export default function Header() {
     }, [])
 
     return (
-        <header ref={navbar} className={"w-full bg-primary-100 bg-opacity-0 flex justify-between py-1 md:py-2 text-xl md:text-2xl lg:text-3xl fixed z-[9999] after:w-full after:h-px after:absolute after:bottom-0 backdrop-blur-sm " + styles.base}>
-            <h1 className="pl-6"><a href="/">{headerConfig.title}</a></h1>
-            <nav className="hidden md:inline pr-6">
-                <ul className="flex gap-6">
+        <header ref={navbar} className={"w-full bg-primary-100 bg-opacity-0 flex justify-between py-1 md:py-2 text-lg md:text-xl lg:text-2xl fixed z-[9999] after:w-full after:h-px after:absolute after:bottom-0 backdrop-blur-sm " + styles.base}>
+            <h1 className="pl-3 md:pl-6"><a href="/">{headerConfig.title}</a></h1>
+            <nav className="hidden md:inline md:pr-6 pr-3">
+                <ul className="flex gap-6 group">
                     {headerConfig.links.map(e=>(
-                        <li key={e.name}><Link href={e.href}>{e.name}</Link></li>
+                        <li className="hover:!text-white-900 group-hover:text-white-300 transition-[color]" key={e.name}><Link href={e.href}>{e.name}</Link></li>
                     ))}
                 </ul>
             </nav>
