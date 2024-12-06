b0f318b8b678f25592f7310d497680aca9a618d1
WorldWideTurtle <148323035+WorldWideTurtle@users.noreply.github.com>
2024-12-06 10:07:14 +0100

    Fix lotus: use css insted of inline transform-origin

--- src/components/lotus.tsx
+++ src/components/lotus.tsx
@@ -4,7 +4,9 @@ export default function Lotus() {
     return (
         <svg width="50" viewBox="-15 -6 35 24" xmlns="http://www.w3.org/2000/svg" fill="pink">
             {new Array(6).fill(0).map((_,i)=>
-                <path key={i} d={leafPath} transform={`translate(2.5 -4) rotate(${i*60})`} transform-origin="0 10"/>
+                <path key={i} d={leafPath} transform={`translate(2.5 -4) rotate(${i*60})`} style={{
+                    transformOrigin: "0 10px"
+                }}/>
             )}
             <circle cx="2.5" cy="6" r="1.5"></circle>
         </svg>
